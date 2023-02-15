17403a0f44e2e7dd4c7fcca98d70780a79ea84a4
Milton Sánchez <sanchezgonzalezmilton@gmail.com>
2023-02-15 13:30:02 -0300

    estilos en form y agregando tarjetas

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,23 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'linear-gradient': '#6448fe',
+        'light-grayish-violet': '#dedddf',
+        'dark-grayish-violet': '#8e8593',
+        'very-dark-violet': '#21092f'
+      },
+      fontFamily: {
+        'space-grotesk': ['Space Grotesk','sans-serif']
+      },
+      backgroundImage: {
+        'main-mobile': "url('/bg-main-mobile.png')",
+        'main-desktop': "url('/bg-main-desktop.png')",
+        'card-back': "url('/bg-card-back.png')",
+        'card-front': "url('/bg-card-front.png')",
+      }
+    },
   },
   plugins: [],
 }
